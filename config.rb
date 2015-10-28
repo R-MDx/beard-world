4eef92f962d4f773af3a90912beb5d3e2fcc7cd9
Roy Melhem <roysgap@hotmail.com>
2015-10-28 12:56:07 +0200

    Fix bug

--- config.rb
+++ config.rb
@@ -31,8 +31,8 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-proxy "/help", "/help.html"
-proxy "/products", "/products.html"
+proxy "beard-world/help", "/help.html"
+proxy "beard-world/products", "/products.html"
 
 # data.works.each do |item|
 #   proxy "/references/#{item.clean}.html", "/work.html", :locals => { :code => item }, :ignore => true
