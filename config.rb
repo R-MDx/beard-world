a601f845ae6736c54d8ac1bd86d85f9b63051c8c
Roy Melhem <roysgap@hotmail.com>
2015-10-28 03:22:29 +0200

    remove ignore build - bug fix on deployment buttons links 404 not found

--- config.rb
+++ config.rb
@@ -31,8 +31,8 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-proxy "/help", "help.html"
-proxy "/products", "products.html"
+proxy "/help", "/help.html"
+proxy "/products", "/products.html"
 
 
 ###
